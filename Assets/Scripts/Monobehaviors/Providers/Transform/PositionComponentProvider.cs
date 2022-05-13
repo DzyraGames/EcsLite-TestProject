947e0c91f0d734f69fb9a04ca097ad3b15e598f2
Nazar Dzyra <n.dzyra27@gmail.com>
2022-05-13 13:04:44 +0300

    ﻿Split TransformComponentProvider

---
+++ Assets/Scripts/Monobehaviors/Providers/Transform/PositionComponentProvider.cs
@@ -0,0 +1,16 @@
+﻿using Leopotam.EcsLite;
+using Voody.UniLeo.Lite;
+
+namespace EcsLiteTestProject.Transform
+{
+    public class PositionComponentProvider : BaseMonoProvider, IConvertToEntity
+    {
+        private EcsPool<PositionComponent> _positionComponentPool;
+        
+        public void Convert(int entity, EcsWorld world)
+        {
+            _positionComponentPool = world.GetPool<PositionComponent>();
+            _positionComponentPool.Add(entity).Position = transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Monobehaviors/Providers/Transform/RotationComponentProvider.cs
@@ -0,0 +1,16 @@
+﻿using Leopotam.EcsLite;
+using Voody.UniLeo.Lite;
+
+namespace EcsLiteTestProject.Transform
+{
+    public class RotationComponentProvider : BaseMonoProvider, IConvertToEntity
+    {
+        private EcsPool<RotationComponent> _rotationComponentPool; 
+        
+        public void Convert(int entity, EcsWorld world)
+        {
+            _rotationComponentPool = world.GetPool<RotationComponent>();
+            _rotationComponentPool.Add(entity).Rotation = transform.rotation;
+        }
+    }
+}
